53104e7feb9832d9019fcd1c8f107ca61b28dd9c
MinoUni <maks.reva.ua@gmail.com>
2024-12-09 17:19:36 +0200

    feat: impl auth service
    Signed-off-by: MinoUni <maks.reva.ua@gmail.com>

---
+++ src/main/java/io/teamchallenge/mentality/customer/CustomerRepository.java
@@ -0,0 +1,12 @@
+package io.teamchallenge.mentality.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+  boolean existsByEmail(String email);
+
+  Integer getCustomerIdByEmail(String email);
+}

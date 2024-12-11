e4db2466e340cb6e7f68c2bde8ba9df36d9717c5
MinoUni <maks.reva.ua@gmail.com>
2024-12-11 16:45:59 +0200

    feat: add customer delete endpoint
    Signed-off-by: MinoUni <maks.reva.ua@gmail.com>

--- src/main/java/io/teamchallenge/mentality/customer/CustomerService.java
+++ src/main/java/io/teamchallenge/mentality/customer/CustomerService.java
@@ -3,10 +3,14 @@ package io.teamchallenge.mentality.customer;
 import io.teamchallenge.mentality.customer.dto.CustomerDto;
 import io.teamchallenge.mentality.exception.CustomerNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CustomerService {
 
   private final CustomerRepository customerRepository;
@@ -16,5 +20,15 @@ public class CustomerService {
     Customer customer =
         customerRepository.findById(id).orElseThrow(() -> new CustomerNotFoundException(id));
     return customerMapper.toCustomerDto(customer);
+  }
+
+  @Transactional
+  public void deleteCustomerById(Integer id) {
+    if (!customerRepository.existsById(id)) {
+      log.info("Customer with id={} not found", id);
+      throw new CustomerNotFoundException(id);
+    }
+    var customer = customerRepository.getReferenceById(id);
+    customerRepository.delete(customer);
   }
 }
--- src/test/resources/io/teamchallenge/mentality/customer/delete-customer.sql
+++
@@ -1,5 +0,0 @@
-DELETE
-FROM mentality_test.public.customers c
-WHERE c.id = 1;
-
-ALTER SEQUENCE customers_id_seq RESTART WITH 1;

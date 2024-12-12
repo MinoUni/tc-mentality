549bec1b7e86a2a51af1c6a1cc47280a0d9b1e03
MinoUni <maks.reva.ua@gmail.com>
2024-12-12 19:35:44 +0200

    fix: rm email update from update dto req
    Signed-off-by: MinoUni <maks.reva.ua@gmail.com>

--- src/main/java/io/teamchallenge/mentality/customer/dto/CustomerPatchDto.java
+++ src/main/java/io/teamchallenge/mentality/customer/dto/CustomerPatchDto.java
@@ -1,7 +1,6 @@
 package io.teamchallenge.mentality.customer.dto;
 
 public record CustomerPatchDto(
-    String email,
     String firstName,
     String lastName,
     String phone,
--- src/main/java/io/teamchallenge/mentality/customer/dto/CustomerUpdateDto.java
+++ src/main/java/io/teamchallenge/mentality/customer/dto/CustomerUpdateDto.java
@@ -1,10 +1,8 @@
 package io.teamchallenge.mentality.customer.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record CustomerUpdateDto(
-    @Email String email,
     @NotBlank String firstName,
     @NotBlank String lastName,
     @NotBlank String phone,

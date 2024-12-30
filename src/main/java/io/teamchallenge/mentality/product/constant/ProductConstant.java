ff9b03c92e1daf2c827a46208861b956d4d8bc83
MinoUni <maks.reva.ua@gmail.com>
2024-12-30 16:03:24 +0200

    fix: resolve checkstyle violation
    Signed-off-by: MinoUni <maks.reva.ua@gmail.com>

--- src/main/java/io/teamchallenge/mentality/product/constant/ProductConstant.java
+++ src/main/java/io/teamchallenge/mentality/product/constant/ProductConstant.java
@@ -1,8 +1,9 @@
 package io.teamchallenge.mentality.product.constant;
 
-public final class ProductConstant {
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ProductConstant {
 
   public static final String PRODUCT_WITH_ID_NOT_FOUND = "Product with id=`{}` not found";
-
-  private ProductConstant() {}
 }

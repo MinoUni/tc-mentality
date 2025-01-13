b9f595b3c0599b533251203e1d23d5565d6d7868
MinoUni <maks.reva.ua@gmail.com>
2025-01-13 17:19:59 +0200

    fix: resolve checkstyle violations
    Signed-off-by: MinoUni <maks.reva.ua@gmail.com>

--- src/main/java/io/teamchallenge/mentality/utils/CustomerConstant.java
+++ src/main/java/io/teamchallenge/mentality/utils/CustomerConstant.java
@@ -5,6 +5,6 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class CustomerConstant {
 
-  public final String CUSTOMER_WITH_ID_NOT_FOUND = "Customer with id={} not found";
-  public final String CUSTOMER_WITH_ID_DELETED = "Customer with id=`{}` deleted";
+  public static final String CUSTOMER_WITH_ID_NOT_FOUND = "Customer with id={} not found";
+  public static final String CUSTOMER_WITH_ID_DELETED = "Customer with id=`{}` deleted";
 }

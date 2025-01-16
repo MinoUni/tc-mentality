5b261ae22605965a89ab0861661f9ce842a0e26b
MinoUni <maks.reva.ua@gmail.com>
2025-01-16 16:23:18 +0200

    add bearerAuth security scheme in swagger config
    Signed-off-by: MinoUni <maks.reva.ua@gmail.com>

--- src/main/java/io/teamchallenge/mentality/config/SwaggerConfig.java
+++ src/main/java/io/teamchallenge/mentality/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package io.teamchallenge.mentality.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
@@ -12,8 +15,24 @@ public class SwaggerConfig {
 
   @Bean
   public OpenAPI openAPI() {
+    final String schemeName = "bearerAuth";
     return new OpenAPI()
         .info(new Info().title("TC-Mentality API").summary("API Documentation").version("1.0.0"))
-        .servers(List.of(new Server().url("http://127.0.0.1:8080").description("local dev env")));
+        .addSecurityItem(new SecurityRequirement().addList(schemeName))
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    schemeName,
+                    new SecurityScheme()
+                        .name(schemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")))
+        .servers(
+            List.of(
+                new Server().url("http://127.0.0.1:8080").description("local dev env"),
+                new Server()
+                    .url("https://tc-mentality.onrender.com")
+                    .description("remote prod env")));
   }
 }
